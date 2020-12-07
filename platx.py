81656ebddab81ec4385720f3a93458455649a473
Mehdi <mabolfathi@gmail.com>
2020-12-07 16:47:48 +0100

    update on 7.12.2020

--- platx.py
+++ platx.py
@@ -7,6 +7,7 @@ from mongodb_module import *
 import time
 from datetime import datetime
 from email_module import *
+from flask import render_template, make_response
 
 app = Flask(__name__)
 app.secret_key = 'somekey'
@@ -18,6 +19,28 @@ logger = logging.getLogger(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def main_clean():
     return render_template('index_basic.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    logger.debug('# in search method ' + str())
+    
+    if flask.request.method == 'POST':
+        logger.debug('# in search method, the POST is sent')
+        print('name is: ', flask.request.form['the_input'])
+
+    return render_template('index_basic.html')
+
+
+@app.route('/_add_numbers', methods=['GET', 'POST'])
+def add_numbers():
+    #a = request.args.get('a', 0, type=int)
+    #b = request.args.get('b', 0, type=int)
+
+    #headers = {'Content-Type': 'text/html'}
+    print('in add_numbers')
+
+    return "<h1>My First Heading</h1><p>My first paragraph.</p>"
 
 if __name__ == "__main__":
     app.logger.setLevel(logging.DEBUG)
