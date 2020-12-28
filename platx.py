707eb203aacd9f9dabd0999f2e138aaa2913d049
Mehdi <mabolfathi@gmail.com>
2020-12-28 20:52:02 +0100

    update 28.12.2020

--- platx.py
+++ platx.py
@@ -25,15 +25,64 @@ def main_clean():
 
 @app.route('/search_backend', methods=['GET', 'POST'])
 def add_numbers():
-    #a = request.args.get('a', 0, type=int)
-    #b = request.args.get('b', 0, type=int)
-    #headers = {'Content-Type': 'text/html'}
-    print('in search_backend')
 
-    results = "some result here"
-    return render_template('search_result.html', results=results)
+    if flask.request.method == 'POST':
 
-    # return "<div style='border: solid blue'><h4>from flask 1</h4><p>from flask 2</p></div>"
+        page = flask.request.form.get('page', 1)
+
+        print('flask username:' +str(flask.request.form.get('user',None)))
+        print('flask keyword:' +str(flask.request.form))
+
+        print('in search_backend, page: '+str(page))
+
+        image_list = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgfO1Mq0Kcpp5TjqGOja-AnEFkpFLAav4R0g&usqp=CAU',
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGqeiMvcMA8ATx6McIgv0QgGq9njL6_9Q9Ww&usqp=CAU',
+                      ]
+
+        results = [
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 14, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 29, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+                'followers': '1593476', 'following': 193,   'img': image_list[2]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+                'followers': '1593476', 'following': 193,   'img': image_list[2]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+                'followers': '42345', 'following': 112, 'img': image_list[0]},
+            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+                'followers': '86', 'following': 242,        'img': image_list[1]},
+            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+                'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        ]
+        return render_template('search_result.html', results=results)
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
