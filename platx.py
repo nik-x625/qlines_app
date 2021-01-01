e00a593903bd29bd67f9b05b08aa6ab51181a59c
Mehdi <mabolfathi@gmail.com>
2021-01-01 18:09:34 +0100

    update 31.12.2020

--- platx.py
+++ platx.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 import logging
 import logging.config
-import flask
-from flask import Flask, render_template, json, jsonify, request, make_response, current_app, abort
-from mongodb_module import *
 import time
+import os
+
+from flask import Flask, render_template, json, jsonify, request, make_response, current_app
+from flask import abort, render_template, make_response, Flask, render_template, request, current_app
+from flask_pager import Pager
+
+from mongodb_module import *
 from datetime import datetime
 from email_module import *
-from flask import render_template, make_response
-
 
 app = Flask(__name__)
-app.secret_key = 'somekey'
+app.secret_key = os.urandom(42)
+app.config['PAGE_SIZE'] = 10
+app.config['VISIBLE_PAGE_COUNT'] = 5
+
 
 logging.config.fileConfig(fname='logging_config.ini',
                           disable_existing_loggers=False)
@@ -24,65 +29,135 @@ def main_clean():
 
 
 @app.route('/search_backend', methods=['GET', 'POST'])
-def add_numbers():
+def search_backend():
 
-    if flask.request.method == 'POST':
+    image_list = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgfO1Mq0Kcpp5TjqGOja-AnEFkpFLAav4R0g&usqp=CAU',
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGqeiMvcMA8ATx6McIgv0QgGq9njL6_9Q9Ww&usqp=CAU',
+                  ]
 
-        page = flask.request.form.get('page', 1)
+    results = [
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 14, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 29, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
+            'followers': '42345', 'following': 112, 'img': image_list[0]},
+        {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
+            'followers': '86', 'following': 242,        'img': image_list[1]},
+        {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
+            'followers': '1593476', 'following': 193,   'img': image_list[2]},
+    ]
 
-        print('flask username:' +str(flask.request.form.get('user',None)))
-        print('flask keyword:' +str(flask.request.form))
+    page = request.form.get('page', 1)
 
-        print('in search_backend, page: '+str(page))
+    if not page.isdigit():
+        page = 1
+    page = int(page)
 
-        image_list = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgfO1Mq0Kcpp5TjqGOja-AnEFkpFLAav4R0g&usqp=CAU',
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGqeiMvcMA8ATx6McIgv0QgGq9njL6_9Q9Ww&usqp=CAU',
-                      ]
+    count = len(results)
+    pager = Pager(page, count)
+    pages = pager.get_pages()
 
-        results = [
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 14, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 29, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
-                'followers': '1593476', 'following': 193,   'img': image_list[2]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
-                'followers': '1593476', 'following': 193,   'img': image_list[2]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 32, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 43, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname1', 'id': 'testid1', 'avglikesppost': 55, 'postspw': 12,
-                'followers': '42345', 'following': 112, 'img': image_list[0]},
-            {'name': 'testname2', 'id': 'testid2', 'avglikesppost': 67, 'postspw': 32,
-                'followers': '86', 'following': 242,        'img': image_list[1]},
-            {'name': 'testname3', 'id': 'testid3', 'avglikesppost': 79, 'postspw': 5,
-                'followers': '1593476', 'following': 193,   'img': image_list[2]},
-        ]
-        return render_template('search_result.html', results=results)
+    skip = (page - 1) * current_app.config['PAGE_SIZE']
+    limit = current_app.config['PAGE_SIZE']
+    data_to_show = results[skip: skip + limit]
+
+    return render_template('search_result.html', results=data_to_show, pages=pages)
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
