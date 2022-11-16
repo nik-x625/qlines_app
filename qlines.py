b39d459c4d1d08ec703076c24401a088fef6f04c
Mehdi <mabolfathi@gmail.com>
2022-11-16 19:19:07 +0100

    day commit

--- qlines.py
+++ qlines.py
@@ -2,6 +2,7 @@
 from rq import Queue
 import redis
 import os
+import requests
 
 from datetime import datetime as dt
 
@@ -53,6 +54,59 @@ users = [User(id) for id in range(1, 5)]
 def index():
     logger.debug('in flask, route is /')
     return render_template('index.html')
+
+
+
+
+
+
+# highcharts test page, from: https://github.com/soumilshah1995/Stockchart-highchart-flask-
+@app.route('/test', methods=['GET', 'POST'])
+def index_test():
+    logger.debug('in flask, route is /test')
+    return render_template('test_mychart.html')
+
+## to fetch data from jquery, highcharts
+@app.route('/fetchdata', methods=["GET", "POST"])
+def fetchdata():
+    payload = {}
+    headers = {}
+    logger.debug('# the fetching data api called')
+    #url = "https://demo-live-data.highcharts.com/aapl-ohlcv.json"
+    #r = requests.get(url, headers=headers, data ={})
+    #logger.debug('# type of r - 1: '+str(type(r)))
+    #r = r.json()
+    #logger.debug('# type of r - 2: '+str(type(r)))
+    #logger.debug('# r is: '+str(r))
+    '''data = [{
+                name: 'Installation & Developers',
+                data: [43934, 48656, 65165, 81827, 112143, 142383,
+                    171533, 165174, 155157, 161454, 154610]
+            }, {
+                name: 'Manufacturing',
+                data: [24916, 37941, 29742, 29851, 32490, 30282,
+                    38121, 36885, 33726, 34243, 31050]
+            }, {
+                name: 'Sales & Distribution',
+                data: [11744, 30000, 16005, 19771, 20185, 24377,
+                    32147, 30912, 29243, 29213, 25663]
+            }, {
+                name: 'Operations & Maintenance',
+                data: [null, null, null, null, null, null, null,
+                    null, 11164, 11218, 10077]
+            }, {
+                name: 'Other',
+                data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                    17300, 13053, 11906, 10073]
+            }]'''
+    
+    data = {'name':'some name here', 'data':[100, 150, 70, 190, 25]}
+    return {"res":data}
+
+
+
+
+
 
 
 # somewhere to login
