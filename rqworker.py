ef8a93fb78cd7d54c1d5bd552b9639dbdce5b219
Mehdi Abolfathi <mabolfathi@gmail.com>
2025-01-03 12:41:52 +0000

    separated rqworker to a separate container

--- rqworker.py
+++ rqworker.py
@@ -7,7 +7,7 @@ from logger_custom import get_module_logger
 logger = get_module_logger(__name__)
 
 listen = ['app1']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redis:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
